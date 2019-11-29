137f2fe6cb3846f8a46d9e3a1ad694ff5e3788b1
MichaelPlasser <michael.plasser@drei.at>
2019-11-29 11:14:48 +0100

    SelectedTSPSpec drawn with Canvas

---
+++ TSP/TSPSpec.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TSPSolver.TSP
+{
+    public class TSPSpec
+    {
+        public TSPSpec(string name, IReadOnlyList<int> optTour, IReadOnlyList<Location> cities)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException("Name must not be null or whitespace.", nameof(name));
+            }
+
+            Name = name;
+            OptTour = optTour;
+            Cities = cities;
+        }
+
+        public string Name { get; }
+
+        public IReadOnlyList<int> OptTour { get; }
+
+        public IReadOnlyList<Location> Cities { get; }
+    }
+
+    public class Location
+    {
+        public Location(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public double X { get; }
+        public double Y { get; }
+    }
+}
